f67c21cd9e5150015c9a58aa5eec914323439a65
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-04-12 15:54:30 +0200

    1

---
+++ Line Continuation characters in python.py
@@ -0,0 +1,29 @@
+
+
+'''
+
+Day 1: Line continuation characters in Python
+
+'''
+
+print("I know Python and its very easy for everyone.")
+
+
+print("------------------------------------------")
+
+
+#print("I know python and
+#     its very easy for everyone.")
+
+
+#--->SyntaxError
+
+
+
+print("------------------------------------------")
+
+
+# In python, a backslash ( \ ) is a continuation character
+
+print("I know python and \
+its very easy for everyone.")
